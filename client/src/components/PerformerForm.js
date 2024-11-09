2ea8ad0b44c56f66636073aceaa639231c0fbdc8
John Mark Harrell <hijohnmark@gmail.com>
2024-11-09 19:20:26 +0700

    add performerform component

---
+++ client/src/components/PerformerForm.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react"
+import { useOutletContext } from "react-router-dom"
+
+const PerformerForm = () => {
+    return (
+        <h1>This is the performer form!</h1>
+    )
+}
+
+export default PerformerForm

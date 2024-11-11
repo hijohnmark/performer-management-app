eddb5629b49112307e1dfe9f3e861608d76ea8c7
John Mark Harrell <hijohnmark@gmail.com>
2024-11-11 17:46:55 +0700

    complete one-to-many relationships in models.py

--- server/models.py
+++ server/models.py
@@ -17,7 +17,7 @@ class Performer(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    type = db.relationship('PerformerType')
+    type = db.relationship('PerformerType', back_populates='performers')
     # type_name = association_proxy('type', 'name')
     
     def __repr__(self):
@@ -29,8 +29,44 @@ class PerformerType(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     
+    performers = db.relationship('Performer', back_populates='type')
+
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     def __repr__(self):
         return f'<Performer type {self.id}: {self.name}>'
+    
+class Event(db.Model, SerializerMixin):
+    __tablename__ = 'events'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    date = db.Column(db.Date)
+    time = db.Column(db.Time)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'))
+
+    venue = db.relationship('Venue', back_populates='events')
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        venue_name = self.venue.name if self.venue else "Unknown Venue"
+        return f'<Event {self.id}: {self.name}, Date: {self.date}, Time: {self.time}, Venue: {venue_name}>'
+    
+class Venue(db.Model, SerializerMixin):
+    __tablename__ = 'venues'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+    capacity = db.Column(db.Integer)
+
+    events = db.relationship('Event', back_populates = 'venue')
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f'<Venue {self.id}: {self.name}, Address: {self.address}, Capacity: {self.capacity}>'
